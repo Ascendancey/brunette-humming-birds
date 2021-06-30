a0d8e59d224b108d8b6654fa96ad7da28348a4e5
Ascendancey <mikhail.petukhov@yahoo.com>
2021-06-30 17:52:47 +0300

    Code cleanup

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import "./SearchBar.css";
+
+const SearchBar = (props) => {
+
+  return (
+    <div className="">
+        <input></input>
+    </div>
+  );
+};
+
+export default SearchBar;
