88eda4e84155752cc26bc3ac1ccecf7f409e6405
Ascendancey <mikhail.petukhov@yahoo.com>
2021-07-04 18:03:40 +0300

    Example star rating added to Ranking List

--- src/pages/RankingList.js
+++ src/pages/RankingList.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 
 import SearchBar from "../components/SearchBar";
+import StarRating from "../components/StarRating";
 
 const RankingList = (props) => {
   return (
     <div>
       <div className="App-content">
         <SearchBar searchHandler={props.searchHandler} dummy={props.dummy}/>
+        <p>Example star rating:</p>
+        <StarRating numberOfStars="4" />
         <h1>Ranking list</h1>
       </div>
     </div>
