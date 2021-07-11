a9a56c19e9761ace1e5dca034528c26778fc3c55
Ascendancey <mikhail.petukhov@yahoo.com>
2021-07-11 14:40:31 +0300

    Search and filters styled

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
@@ -85,58 +92,189 @@ const SearchBar = (props) => {
     setFilters(false);
   };
 
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+  const classes = useStyles();
+
+  const useStylesInput = makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "80%",
+      },
+    },
+  }));
+
+  const classesInput = useStylesInput();
+
   return (
     <div className="SearchBar">
-      <input className="SearchBar-input" onChange={inputHandler}></input>
-      <button className="SearchBar-button" onClick={clickHandler}>
-        Search
-      </button>
+      <div className="bigreak" />
+      <div className="SearchField">
+        <TextField
+          className="SearchBar-input"
+          id="standard-basic"
+          label="What are you looking for?"
+          onChange={inputHandler}
+        />
+
+        <button className="SearchBar-button" onClick={clickHandler}>
+          Search
+        </button>
+      </div>
+
+      <div className="bigreak" />
+
       {!isFilters && (
-        // <button onClick={showFilters}>Show filters</button>
-        <img className="userImage" onClick={showFilters} src="../image/expand_blue.png" />
+        <img
+          className="showFilters"
+          onClick={showFilters}
+          src="../image/expand_blue.png"
+        />
       )}
       {isFilters && (
         <div className="SearchBar-filters">
-          <select onChange={setInputLang}>
-            {uniqueLangs.map((item, i) => {
-              return <option key={i}> {item} </option>;
-            })}
-          </select>
-          <select onChange={setInputCity}>
-            {props.dummy.map((item, i) => {
-              return <option key={i}> {item.location.city} </option>;
-            })}
-          </select>
-          <select onChange={setInputSpec}>
-            {props.dummy.map((item, i) => {
-              return <option key={i}> {item.specialization} </option>;
-            })}
-          </select>
-          <label>Price range:</label>
-          <select onChange={setInputPrice}>
-            {props.dummy.map((item, i) => {
-              return <option key={i}> {item.price} </option>;
-            })}
-          </select>
-          <label>Rating:</label>
-          <select onChange={setInputRating}>
-            {props.dummy.map((item, i) => {
-              return <option key={i}> {item.rating} </option>;
-            })}
-          </select>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Language
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={inputValue.language}
+              onChange={setInputLang}
+              label="Language"
+            >
+              {uniqueLangs.map((item, i) => {
+                return (
+                  <MenuItem key={i} value={item}>
+                    {item}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
 
-          <label>Available in (hours):</label>
-          <select onChange={setInputAvailability}>
-            {props.dummy.map((item, i) => {
-              return <option key={i}> {item.availablein} </option>;
-            })}
-          </select>
-          <br />
-          <img className="userImage" onClick={hideFilters} src="../image/expand_blue_upsidedown.png" />
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">City</InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={inputValue.city}
+              onChange={setInputCity}
+              label="City"
+            >
+              {props.dummy.map((item, i) => {
+                return (
+                  <MenuItem key={i} value={item.location.city}>
+                    {item.location.city}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Specialization
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={inputValue.specialization}
+              onChange={setInputSpec}
+              label="Specialization"
+            >
+              {props.dummy.map((item, i) => {
+                return (
+                  <MenuItem key={i} value={item.specialization}>
+                    {item.specialization}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+
+          <div className="break" />
+
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Price
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={inputValue.price}
+              onChange={setInputPrice}
+              label="City"
+            >
+              {props.dummy.map((item, i) => {
+                return (
+                  <MenuItem key={i} value={item.price}>
+                    {item.price}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Rating
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={inputValue.rating}
+              onChange={setInputRating}
+              label="Rating"
+            >
+              {props.dummy.map((item, i) => {
+                return (
+                  <MenuItem key={i} value={item.rating}>
+                    {item.rating}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Available in
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={inputValue.availability}
+              onChange={setInputAvailability}
+              label="Available in"
+            >
+              {props.dummy.map((item, i) => {
+                return (
+                  <MenuItem key={i} value={item.availablein}>
+                    {item.availablein}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          {/* </div> */}
+          <div className="break" />
+          <img
+            className="showFilters"
+            onClick={hideFilters}
+            src="../image/expand_blue_upsidedown.png"
+          />
         </div>
       )}
-
-      <div></div>
     </div>
   );
 };
